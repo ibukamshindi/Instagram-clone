c3c93bf601b7e73e7eb0a54b8620ed3bdb9e09ab
ibukamshindi <ibukamshindi@gmail.com>
2020-10-19 19:41:25 +0300

    update views

--- Instagram/urls.py
+++ Instagram/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.conf.urls import url,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path(r'^IG/',include('IG.urls')),
+    url(r'admin/', admin.site.urls),
+    url(r'', include('IG.urls')),
 ]
