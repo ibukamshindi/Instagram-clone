96016094e2ec8c78c2207c99cf14296552ac45b2
ibukamshindi <ibukamshindi@gmail.com>
2020-10-19 04:48:53 +0300

    add test for profile change model migrate

--- IG/models.py
+++ IG/models.py
@@ -1,15 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
-from pyuploadcare.dj.models import ImageField
+from django.db import transaction
 
 # Create your models here.
 class Profile(models.Model):
-      profile_photo=ImageField(blank=True)
+      profile_photo=models.ImageField(upload_to='profiles/',null=True)
       bio=models.TextField()
-      user=models.OneToOneField(User,on_delete=models.CASCADE)
+      user=models.ForeignKey(User,on_delete=models.CASCADE)
 
+
+      def __str__(self):
+         return self.user.username
+
+      def save_profile(self):
+          self.save()
+      
 class Image(models.Model):
-    image=ImageField(manual_crop="1080x800",null=True)
+    image=models.ImageField(upload_to='photos/',null=True)
     image_name=models.CharField(max_length =30,null=True)
     image_caption=models.TextField()
     profile=models.ForeignKey(Profile,null=True,on_delete=models.CASCADE)
--- IG/tests.py
+++ IG/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Image, Profile,Comment
+from django.contrib.auth.models import User
+
+# Create your tests here.
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        user=User(username='pato')
+        self.profile=Profile(profile_photo='this is me',bio='execeptional',user=user)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile,Profile))
+
+    def test_save_method(self):
+        self.profile.save_profile()
+        profiles=Profile.objects.all()
+        self.assertTrue(len(profiles)>0)
+
+    
+
