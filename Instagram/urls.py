940f54b4048d112da0b440c563e4f760115bca69
ibukamshindi <ibukamshindi@gmail.com>
2020-10-21 02:58:08 +0300

    add django registration static files registration templates

--- Instagram/urls.py
+++ Instagram/urls.py
@@ -20,4 +20,5 @@ from django.conf.urls import url,include
 urlpatterns = [
     url(r'admin/', admin.site.urls),
     url(r'', include('IG.urls')),
+    url(r'^accounts/', include('django_registration.backends.one_step.urls')),
 ]
