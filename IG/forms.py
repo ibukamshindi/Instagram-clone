150b8083b6310b0682b6ce4fe66083656108f1d8
ibukamshindi <ibukamshindi@gmail.com>
2020-10-22 13:56:01 +0300

    db changes

---
+++ IG/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Profile,Image,Comment
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+      model=Profile
+      exclude=['user']
+
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude =['likes','profile']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        exclude = ['image', 'user']
